ad757108b974c3c60d850d1448a7e1f4d888ef47
Jungho Park <junghopark@Junghoui-MacBookPro.local>
2020-03-25 17:34:50 -0700

    assignment 8 commit

---
+++ pythonclubapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('getresources/', views.getresources, name='types'),
+    path('getmeetings/', views.getmeetings, name='meeting'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='meetingid'),
+    path('newMeeting/', views.newMeeting, name='newmeeting'),
+]
